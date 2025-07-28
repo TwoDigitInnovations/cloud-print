9b711c39ae1d9a708bff0656fa6bdb1a210959d5
Chetan7528 <kaklotarchetan7528@gmail.com>
2025-07-28 22:42:03 +0530

    minor change

--- electron/main.cjs
+++ electron/main.cjs
@@ -64,66 +64,30 @@ function createWindow() {
             // withCredentials: true,
         });
         const url = 'https://res.cloudinary.com/dweddwpll/image/upload/v1753082255/properties/brex6cfdjbvnwszq6uzr.png'
-        // const options = {
-        //     string: true,
-        //     headers: {
-        //         "User-Agent": "my-app"
-        //     }
-        // };
-        // const image = await encode(url, options);
+
         const { base64, contentType } = await downloadImageAsBase64WithSize(url);
         const format = contentType.split('/')[1].toUpperCase(); // JPEG, PNG, etc.
-        console.log(contentType, format)
-        // console.log('data:image/jpg;base64,' + base64)
-        const doc = new jsPDF();
-        const imgWidth = 190;
-        const imgHeight = (imgWidth * 3) / 4;
-        doc.addImage(`data:${contentType};base64,${base64}`, format, 5, 5, 190, 260);
-        // console.log(doc)
-        // Get PDF as Blob
-        const tempPath = path.join(os.tmpdir(), new Date().getTime() + '.pdf');
-        fs.writeFileSync(tempPath, doc.output());
 
-        const fileUrl = 'file://' + tempPath;
-        console.log('PDF File URL:', fileUrl);
-        // console.log(image)
         const documentDefinition = {
             content: [
                 {
                     image: 'data:image/png;base64,' + base64,
-                    // width: 500 // Adjust as needed
+                    width: 510 // Adjust as needed
                 }
             ]
         };
 
-        //           const imageData = fs.readFileSync(imagePath);
-        //   const base64Image = imageData.toString('base64');
-
-        //         const doc = new jsPDF();
-        //         if (typeof window !== 'undefined') {
-        //             const img = new window.Image();
-        //             img.src = url;
-        //             img.onload = () => {
-        //                 const imgWidth = 190; // PDF page width (210mm) with margin
-        //                 const imgHeight = (img.height * imgWidth) / img.width;
-
-        //                 doc.addImage('data:image/jpg;base64,' + image, 'JPEG', 10, 10, imgWidth, imgHeight);
-        //                 // Get PDF as Blob
-        //                 const pdfBlob = doc.output('blob');
-
-        //                 // Create URL
-        //                 const pdfUrl = URL.createObjectURL(pdfBlob);
-        //                 console.log(pdfUrl)
-
-        //                 //   doc.save('image.pdf');
-        //             };
-        //         }
 
 
-        pdfMake.createPdf(documentDefinition).getDataUrl(async (result) => {
-            console.log(result.charAt(0))
+
+        pdfMake.createPdf(documentDefinition).getBuffer(async (result) => {
+            // console.log(result.charAt(0))
+            const tempPath = path.join(os.tmpdir(), new Date().getTime() + '.pdf');
+            fs.writeFileSync(tempPath, result);
+            const fileUrl = 'file://' + tempPath;
+            console.log('PDF File URL:', fileUrl);
             try {
-                await printer.print(result, {
+                await printer.print(fileUrl, {
                     printer: 'EPSON CW-C4000 Series',
                     unix: ['-o media=Custom.85x54mm', '-o fit-to-page'], // macOS/Linux print options
                     win32: ['-print-settings "fit"'], // Windows-specific options
